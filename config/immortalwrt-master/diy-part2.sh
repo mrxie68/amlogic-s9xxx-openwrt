5e0a89d93d3f6601e33485a8d0f3d0c0df6ca76e
mrxie68 <mrxie6@gmail.com>
2024-10-08 21:25:57 +0800

    Initial commit

--- config/immortalwrt-master/diy-part2.sh
+++ config/immortalwrt-master/diy-part2.sh
@@ -16,15 +16,24 @@ sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='R$(date +%Y.%m.%d)'|g" package
 echo "DISTRIB_SOURCECODE='immortalwrt'" >>package/base-files/files/etc/openwrt_release
 
 # Modify default IP（FROM 192.168.1.1 CHANGE TO 192.168.31.4）
-# sed -i 's/192.168.1.1/192.168.31.4/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.10.10.23/g' package/base-files/files/bin/config_generate
 #
 # ------------------------------- Main source ends -------------------------------
 
 # ------------------------------- Other started -------------------------------
 #
-# Add luci-app-amlogic
+# Remove packages
+rm -rf feeds/packages/net/v2ray-geodata
+rm -rf feeds/packages/lang/golang
 rm -rf package/luci-app-amlogic
+# Add packages
 git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
+
+sed -i '/\/etc\/init\.d\/tailscale/d;/\/etc\/config\/tailscale/d;' feeds/packages/net/tailscale/Makefile
+git clone https://github.com/asvow/luci-app-tailscale package/luci-app-tailscale
 #
 # Apply patch
 # git apply ../config/patches/{0001*,0002*}.patch --directory=feeds/luci
@@ -38,47 +47,5 @@ echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.defa
 
 
 
-#!/bin/bash
 
-# 切换到 openwrt 目录
-cd openwrt/
-# 删除 feeds 中的 golang 包
-echo "正在删除 feeds 中的 golang 包..."
-rm -rf feeds/packages/lang/golang
 
-# 克隆 golang 包的指定分支
-echo "正在克隆 golang 包..."
-git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
-
-# 删除 feeds 中的 v2ray-geodata 包（适用于 openwrt-22.03 和 master 分支）
-echo "正在删除 feeds 中的 v2ray-geodata 包..."
-rm -rf feeds/packages/net/v2ray-geodata
-
-# 克隆 mosdns 和 v2ray-geodata 的仓库
-echo "正在克隆 mosdns 和 v2ray-geodata 仓库..."
-git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
-
-# 在 .config 中添加 mosdns 配置
-echo "CONFIG_PACKAGE_luci-app-mosdns=y" >> .config
-echo "CONFIG_PACKAGE_luci-i18n-mosdns-zh-cn=y" >> .config
-
-# 编译 mosdns 包
-echo "正在编译 mosdns 包..."
-make package/mosdns/luci-app-mosdns/compile V=s
-
-# 修改 tailscale Makefile，删除与 /etc/init.d/tailscale 和 /etc/config/tailscale 相关的行
-echo "正在修改 tailscale 的 Makefile..."
-sed -i '/\/etc\/init\.d\/tailscale/d;/\/etc\/config\/tailscale/d;' feeds/packages/net/tailscale/Makefile
-
-# 克隆 luci-app-tailscale 仓库
-echo "正在克隆 luci-app-tailscale 仓库..."
-git clone https://github.com/asvow/luci-app-tailscale package/luci-app-tailscale
-
-# 在 .config 中添加 tailscale 配置
-echo "CONFIG_PACKAGE_luci-app-tailscale=y" >> .config
-echo "CONFIG_PACKAGE_luci-i18n-tailscale-zh-cn=y" >> .config
-
-# 编译 luci-app-tailscale 包
-echo "正在编译 luci-app-tailscale 包..."
-make package/luci-app-tailscale/compile V=s
